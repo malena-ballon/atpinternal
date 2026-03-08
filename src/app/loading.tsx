fdd387bb5af341ac26b8bd171b59694be3914fe4
malena-ballon <malenarcballon@gmail.com>
2026-03-08 22:50:04 +0800

    Loading State

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
+      {/* 1. Circle is first */}
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
+      
+      {/* 2. Text is second (appears below) */}
+      <p className="text-lg font-small text-white-600 animate-pulse">waityyyyyy!!!</p>
+    </div>
+  );
+}
